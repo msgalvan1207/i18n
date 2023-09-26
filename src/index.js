67a5ed20ec65397b049f7041dbc5c7393daddbd9
msgalvan1207 <ms.galvan@uniandes.edu.co>
2023-09-26 16:16:21 -0500

    Trabajo adelantado

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { IntlProvider } from "react-intl";
+
+import localeEsMessages from "./locales/es.json";
+import localeEnMessages from "./locales/en.json";
 
 import JobsList from "./components/jobslist";
 
-ReactDOM.render(<JobsList />, document.getElementById("root"));
+const det = () => {
+    if (navigator.language ==='en'){
+        return localeEnMessages
+    } else if (navigator.language==='es') {
+        return localeEsMessages
+    }
+}
+
+ReactDOM.render(
+
+    <IntlProvider locale={navigator.language} messages={det()}>
+        <JobsList />
+    </IntlProvider>, document.getElementById("root"));
